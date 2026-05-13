21c2d73b868e8e7e50a03687892c07dd0125932c
edopt1214-glitch <edopt1214@gmail.com>
2026-05-13 13:17:09 +0800

    Delete snakes-and-ladders directory

--- snakes-and-ladders/js/core/dice.js
+++
@@ -1,5 +0,0 @@
-import { randInt } from "./rng.js";
-
-export function rollDie(rng) {
-  return randInt(rng, 1, 6);
-}
--- snakes-and-ladders/js/systems/eventSystem.js
+++
@@ -1,9 +0,0 @@
-import { maybeFireRandomEvent, fireEventById } from "../../data/events.js";
-
-export function tickEvents(state) {
-  return maybeFireRandomEvent(state);
-}
-
-export function triggerEvent(state, id) {
-  return fireEventById(state, id);
-}
